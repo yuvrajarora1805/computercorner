c8bdf8717084e4a9099a6a7b42a24354c83e6045
Yuvraj Arora <yuvrajarora1805@github.com>
2026-09-16 14:17:13 +0530

    Ignore ESLint errors during Next.js build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ["i.ibb.co", "images.unsplash.com", "encrypted-tbn3.gstatic.com", "www.gigabyte.com", "static.gigabyte.com"],
   },
