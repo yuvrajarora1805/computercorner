df4cc58560c5ca745f9665237c89127f07a54107
Yuvraj Arora <yuvrajarora1805@github.com>
2026-09-16 14:24:53 +0530

    Fix module import paths for db utility

--- src/pages/api/admin/prebuilt-pcs/[id].js
+++ src/pages/api/admin/prebuilt-pcs/[id].js
@@ -1,4 +1,4 @@
-import pool from '../../../../../utils/db';
+import pool from '@/utils/db';
 
 export default async function handler(req, res) {
   const { id } = req.query;
--- src/pages/api/admin/prebuilt-pcs/index.js
+++ src/pages/api/admin/prebuilt-pcs/index.js
@@ -1,4 +1,4 @@
-import pool from '../../../../utils/db';
+import pool from '@/utils/db';
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
